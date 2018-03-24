bca95186d09b62ede2bc273f7374c7476358fa09
Ting <tingalex30@gmail.com>
2018-03-24 14:03:19 +0800

    Initial Commit

---
+++ WeChatCode/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WeChatCode
+//
+//  Created by OurEDA on 2018/3/24.
+//  Copyright © 2018年 OurEDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
